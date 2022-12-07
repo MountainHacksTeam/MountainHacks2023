d6eb1a4a169f129568c30b167d95958652ae9ed8
Akze <57077182+the-akze@users.noreply.github.com>
2022-12-07 17:00:10 +0000

    sponsor page, deleted info page bc that is just main page, register page, added "email updates" input (not functional yet)

---
+++ dbConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBUX8FHhprnhVHK8dYmAR68p7dQ3PuLMC8",
+    authDomain: "mountainhacks-df632.firebaseapp.com",
+    projectId: "mountainhacks-df632",
+    storageBucket: "mountainhacks-df632.appspot.com",
+    messagingSenderId: "843377316450",
+    appId: "1:843377316450:web:9668737ca880d230e4b73b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
